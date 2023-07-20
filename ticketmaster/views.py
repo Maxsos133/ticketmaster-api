4eb76b501c90684dbf551b3c9ac1e6ae3148cfb0
Maxsos133 <max.pavlenko133@gmail.com>
2023-07-20 12:49:55 -0500

    config cors

--- ticketmaster/views.py
+++ ticketmaster/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
 from .serializers import VenueSerializer, EventSerializer
 from .models import Venue, Event
 
@@ -16,20 +15,6 @@ class EventList(generics.ListCreateAPIView):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
 
-from rest_framework import generics
-
 class EventDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
-    permission_classes = [IsAuthenticated]
-
-    def put(self, request, *args, **kwargs):
-        partial = kwargs.pop('partial', False)
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=partial)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-
-        # Perform any additional actions here, such as incrementing tickets_sold
-
-        return Response(serializer.data)
